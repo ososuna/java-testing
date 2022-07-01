84dbbc3e032a5ada611fd3a949c94d696727593d
oswi <oswaldo.adrian@live.com.mx>
2022-06-30 22:35:31 -0600

    string util testing with junit

--- java-testing/src/test/java/com/platzi/utils/StringUtilTest.java
+++ java-testing/src/test/java/com/platzi/utils/StringUtilTest.java
@@ -1,15 +1,15 @@
 package com.platzi.utils;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
 public class StringUtilTest {
   
-  public static void main(String[] args) {
+  @Test 
+  public void testRepeat() {
     assertEquals(StringUtil.repeat("Hola", 3), "HolaHolaHola");
     assertEquals(StringUtil.repeat("Hola", 1), "Hola");
   }
 
-  private static void assertEquals(String actual, String expected) {
-    if(!actual.equals(expected)) {
-      throw new RuntimeException(actual + " is not equal to " + expected);
-    }
-  }
 }
