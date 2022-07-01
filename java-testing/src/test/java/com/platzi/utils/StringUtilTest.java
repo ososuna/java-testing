f398c77c6e50749563493fe96bd0344a634ac57c
oswi <oswaldo.adrian@live.com.mx>
2022-06-30 22:30:56 -0600

    string util unit test

--- java-testing/src/test/java/com/platzi/utils/StringUtilTest.java
+++ java-testing/src/test/java/com/platzi/utils/StringUtilTest.java
@@ -1,16 +1,15 @@
 package com.platzi.utils;
 
-
 public class StringUtilTest {
   
   public static void main(String[] args) {
-    String result = StringUtil.repeat("Hola", 3);
-    if(!result.equals("HolaHolaHola")) {
-      System.out.println("ERROR");
-    }
-    String result2 = StringUtil.repeat("Hola", 1);
-    if(!result2.equals("Hola")) {
-      System.out.println("ERROR");
+    assertEquals(StringUtil.repeat("Hola", 3), "HolaHolaHola");
+    assertEquals(StringUtil.repeat("Hola", 1), "Hola");
+  }
+
+  private static void assertEquals(String actual, String expected) {
+    if(!actual.equals(expected)) {
+      throw new RuntimeException(actual + " is not equal to " + expected);
     }
   }
 }
