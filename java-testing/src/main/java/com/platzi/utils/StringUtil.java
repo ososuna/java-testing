4507b63c8b8329f39457f7be7df332947e35d4af
oswi <oswaldo.adrian@live.com.mx>
2022-07-04 08:36:38 -0600

    test cases

--- java-testing/src/main/java/com/platzi/utils/StringUtil.java
+++ java-testing/src/main/java/com/platzi/utils/StringUtil.java
@@ -3,6 +3,11 @@ package com.platzi.utils;
 public class StringUtil {
 
   public static String repeat(String str, int times) {
+
+    if (times < 0) {
+      throw new IllegalArgumentException("Times must be greater than or equal to 0");
+    }
+
     StringBuilder sb = new StringBuilder();
     for (int i = 0; i < times; i++) {
       sb.append(str);
--- java-testing/src/test/java/com/platzi/utils/StringUtilTest.java
+++ java-testing/src/test/java/com/platzi/utils/StringUtilTest.java
@@ -6,10 +6,24 @@ import org.junit.Test;
 
 public class StringUtilTest {
   
-  @Test 
-  public void testRepeat() {
-    assertEquals(StringUtil.repeat("Hola", 3), "HolaHolaHola");
+  @Test
+  public void testRepeatZero() {
+    assertEquals(StringUtil.repeat("Hola", 0), "");
+  }
+
+  @Test
+  public void testRepeatOnce() {
     assertEquals(StringUtil.repeat("Hola", 1), "Hola");
+  }
+
+  @Test
+  public void testRepeatMultiple() {
+    assertEquals(StringUtil.repeat("Hola", 3), "HolaHolaHola");
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testRepeatNegative() {
+    StringUtil.repeat("Hola", -1);
   }
 
 }
