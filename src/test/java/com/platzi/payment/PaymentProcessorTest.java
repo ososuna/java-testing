e119d8c34ebe98ca8628664f58dd53738eaa7096
oswi <oswaldo.adrian@live.com.mx>
2022-07-04 21:49:16 -0600

    is wrong test

--- src/test/java/com/platzi/payment/PaymentProcessorTest.java
+++ src/test/java/com/platzi/payment/PaymentProcessorTest.java
@@ -1,5 +1,6 @@
 package com.platzi.payment;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
@@ -21,6 +22,14 @@ public class PaymentProcessorTest {
       .thenReturn(new PaymentResponse(PaymentResponse.PaymentStatus.OK));
     PaymentProcessor paymentProcessor = new PaymentProcessor(paymentGateway);
     assertTrue(paymentProcessor.makePayment(1000));
+  }
+
+  @Test
+  public void paymentIsWrong() {
+    when(paymentGateway.requestPayment(any()))
+      .thenReturn(new PaymentResponse(PaymentResponse.PaymentStatus.ERROR));
+    PaymentProcessor paymentProcessor = new PaymentProcessor(paymentGateway);
+    assertFalse(paymentProcessor.makePayment(1000));
   }
 
 }
